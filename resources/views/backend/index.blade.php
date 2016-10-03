5635733e67cd1cf25c8fd4391f8e398cc4cec3c3
M Arif Mahthunisnaini <mahthunis@gmail.com>
2016-10-03 10:51:29 +0700

    Change style in post, add simplemde editor

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -10,13 +10,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- simplemde -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
   </head>
-  <body>
+  <body class="skin-blue sidebar-mini">
     <app></app>
     <script>
       var CONFIG = {};
       CONFIG.USER = {!! Auth::user() !!}
     </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
     <script src="/js/build.js"></script>
+    <script src="/js/adminlte.js"></script>
   </body>
 </html>
