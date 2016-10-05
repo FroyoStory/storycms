82201a226dfe237f07e502f9b2483f57ee617879
Purwandi <free6300@gmail.com>
2016-10-05 14:24:31 +0700

    Webpack swap #18 (#19)
    * Webpack swap #18
    
    * Update depedencies

--- resources/assets/js/pages/app/index.js
+++ resources/assets/js/pages/app/index.js
@@ -2,12 +2,6 @@ import Http from '../../services/http'
 import Template from './template.jade'
 export let name = 'App'
 
-import '../../../css/ghost-admin.css'
-import '../../../css/bootstrap.css'
-import '../../../less/admin.less'
-import '../../../less/skins/skin-blue.less'
-import '../../../sass/custom.sass'
-
 export default {
   name,
   template: Template(),
